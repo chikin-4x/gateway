23f34704a7038cbfc4c487a25b3eaa9796492f0c
Morgan McEntire <morgan.mcentire@cfacorp.com>
2020-07-01 15:33:40 -0400

    change mac from IP to interface

--- gateway_darwin.go
+++ gateway_darwin.go
@@ -1,15 +1,14 @@
 package gateway
 
 import (
-	"net"
 	"os/exec"
 )
 
-func DiscoverGateway() (net.IP, error) {
+func DiscoverGateway() (string, error) {
 	routeCmd := exec.Command("/sbin/route", "-n", "get", "0.0.0.0")
 	output, err := routeCmd.CombinedOutput()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	return parseDarwinRouteGet(output)
